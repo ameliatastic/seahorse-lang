09093f9c145de900f2a8dadc209d213c4b0a6fd2
✨ amelia chen ✨ <ameliatastic@proton.me>
2022-11-10 16:39:08 -0500

    initial pyth update part 1: adding a new data file for the pyth addresses

--- src/data.rs
+++ src/data.rs
@@ -1,2 +1,5 @@
-// Text data (see /data), included as string constants.
+// Text data (see /data/const), included as string constants.
 include!(concat!(env!("OUT_DIR"), "/data.rs"));
+
+// Pyth price addresses
+include!(concat!(env!("OUT_DIR"), "/pyth.rs"));
