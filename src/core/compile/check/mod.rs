04ced6c80efd152ee978aaa804d2c1df66d089f9
✨ amelia chen ✨ <ameliatastic@proton.me>
2022-11-30 20:14:45 -0500

    Arithmetic bug where transformations weren't being removed

--- src/core/compile/check/mod.rs
+++ src/core/compile/check/mod.rs
@@ -1667,7 +1667,11 @@ impl<'a> Context<'a> {
                 if !assign {
                     let ty = self.check_binop_sides(left, right, loc)?;
 
-                    self.unify(expr_ty, ty, loc)
+                    let ty = self.unify(expr_ty, ty, loc)?;
+                    Ok(match ty {
+                        Ty::Transformed(ty, _) => *ty,
+                        ty => ty,
+                    })
                 } else {
                     let param = self.free();
                     self.check_expr(Ty::Param(param), left)?;
